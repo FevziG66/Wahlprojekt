2384f273eaf9027f5f7e4d328e19a108fe124720
FevziG66 <fevzi.gencer@hotmail.com>
2021-11-10 19:38:28 +0100

    Neuer Aufbau

--- start/views.py
+++ start/views.py
@@ -2,20 +2,22 @@ from django.shortcuts import render
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 # Create your views here.
 def index(request):
-    return render(request, 'start/main.html')
+    context = {"title": "Index"}
+    return render(request, 'start/main.html', context)
+
 
 def register(request):
-    register_form = UserCreationForm()
-    #Wir übergeben die Form an unsere Html Datei
-    return render(request, 'start/register.html', {'register_form': register_form})
+    context = {"title": "Account anlegen"}
+    return render(request, 'start/register.html',context)
 
 def login(request):
-    login_form = AuthenticationForm()
-    #Wir übergeben die Form an unsere Html Datei
-    return render(request, 'start/login.html', {'login_form': login_form})
+    context = {"title": "Anmelden"}
+    return render(request, 'start/login.html',context)
 
 def aboutUs(request):
-    return render(request, 'start/aboutUs.html')
+    context = {"title": "Über uns"}
+    return render(request, 'start/aboutUs.html',context)
 
 def impressum(request):
-    return render(request, 'start/impressum.html')
+    context = {"title": "Impressum"}
+    return render(request, 'start/impressum.html',context)
