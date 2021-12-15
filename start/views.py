76ff8d553ac517e96d36df1f3a4be000c0ab7931
FevziG66 <fevzi.gencer@hotmail.com>
2021-12-15 21:38:30 +0100

    Update

--- start/views.py
+++ start/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 # Create your views here.
+from django.contrib.auth.models import User
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .forms import RegisterForm
+from django.contrib.auth import authenticate, login
 
-def register(request):
-    context = {"title": "Account anlegen"}
-    return render(request, 'start/register.html',context)
+#def register(request):
+#    context = {"title": "Account anlegen"}
+#    return render(request, 'start/register.html',context)
 
-def login(request):
-    context = {"title": "Anmelden"}
-    return render(request, 'start/login.html',context)
+#def login(request):
+#    context = {"title": "Anmelden"}
+#    return render(request, 'start/login.html',context)
 
 def aboutUs(request):
     context = {"title": "Über uns"}
@@ -33,3 +38,68 @@ def resetPassword(request):
 def resetPasswordDone(request):
     context = {"title": "Passwort geändert"}
     return render(request, 'start/resetPasswordDone.html',context)
+def register(request):
+    # if this is a POST request we need to process the form data
+    template = 'start/register.html'
+   
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = RegisterForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            if User.objects.filter(username=form.cleaned_data['username']).exists():
+                return render(request, template, {
+                    'form': form,
+                    'error_message': 'Unternehmen existiert bereits.'
+                })
+            elif User.objects.filter(email=form.cleaned_data['email']).exists():
+                return render(request, template, {
+                    'form': form,
+                    'error_message': 'Email-Adresse existiert bereits.'
+                })
+            elif form.cleaned_data['password'] != form.cleaned_data['password_repeat']:
+                return render(request, template, {
+                    'form': form,
+                    'error_message': 'Passwörter stimmen nicht überein.'
+                })
+            else:
+                # Create the user:
+                user = User.objects.create_user(
+                    form.cleaned_data['username'],
+                    form.cleaned_data['email'],
+                    form.cleaned_data['password']
+                )
+                user.first_name = form.cleaned_data['first_name']
+                user.last_name = form.cleaned_data['last_name']
+                user.save()
+               
+                # Login the user
+                login(request, user)
+               
+                # redirect to accounts page:
+                return HttpResponseRedirect('/home/templates/home/dashboard.html')
+
+   # No post data availabe, let's just show the page.
+    else:
+        form = RegisterForm()
+
+    return render(request, template, {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        # Process the request if posted data are available
+        username = request.POST['username']
+        password = request.POST['password']
+        # Check username and password combination if correct
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            # Save session as cookie to login the user
+            login(request, user)
+            # Success, now let's login the user.
+            return render(request, 'home/dashboard.html')
+        else:
+            # Incorrect credentials, let's throw an error to the screen.
+            return render(request, 'start/login.html', {'error_message': 'Incorrect username and / or password.'})
+    else:
+        # No post data availabe, let's just show the page to the user.
+        return render(request, 'start/login.html')
